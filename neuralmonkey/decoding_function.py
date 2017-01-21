2b82b36b7f786bfd7939450864c53d444f5b59d8
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2017-01-21 13:13:07 +0100

    hierarchical multi-attention

--- neuralmonkey/decoding_function.py
+++ neuralmonkey/decoding_function.py
@@ -61,7 +61,7 @@ class Attention(object):
             self.v_bias = tf.get_variable(
                 "AttnV_b", [], initializer=tf.constant_initializer(0))
 
-    def attention(self, query_state, _a, _b):
+    def attention(self, query_state, prev_state, _):
         """Put attention masks on att_states_reshaped
            using hidden_features and query.
         """
